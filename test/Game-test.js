e1f98240b477507228d5a6ae04a552fee41ad8c4
Sage Vogt <thurmanvogt@gmail.com>
2018-03-20 16:11:13 -0600

    Add game tests for collision detection

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,5 +1,7 @@
 const assert = require('chai').assert;
 const Game = require('./../lib/Game.js');
+const Player = require('./../lib/Player.js');
+const Platform = require('./../lib/Platform.js');
 
 describe ('Game', function() {
     it('should be a function', function() {
@@ -11,13 +13,40 @@ describe ('Game', function() {
         assert.isObject(game);
     })
 
-    it('game should havea  size', function() {
+    it('should have  size', function() {
         var game = new Game({width: 500, height: 700});
         assert.deepEqual(game.gameSize, {width: 500, height: 700});
     })
 
-    it('it should have game pieces', function() {
+    it('should instantiate a player', function() {
         var game = new Game({width: 500, height: 700});
-        assert.equal(game.gamePieces.length, 1)
+        assert.isObject(game.player);
     })
+
+    it('should instantiate 8 platforms', function() {
+        var game = new Game({width: 500, height: 700});
+        assert.equal(game.platforms.length, 8);
+    })
+
+    it('should randomize platform position', function() {
+        var game = new Game({width: 500, height: 700});
+        assert.notEqual(game.platforms[2].position.x, game.platforms[3].position.x);
+    })
+
+    it('should randomize platform position', function() {
+        var game = new Game({width: 500, height: 700});
+        assert.notEqual(game.platforms[2].position.x, game.platforms[3].position.x);
+    })
+
+    it('should set player gravitySpeed to 0 when collision occurs', function() {
+        var game = new Game({width: 500, height: 700});
+        game.player = new Player(15, 15, 15, 15)
+        game.platforms = [new Platform(15, 15, 15, 15)]
+
+        game.collisionDetection()
+
+        assert.equal(game.player.gravitySpeed, 0);
+    })
+    
+
 });
