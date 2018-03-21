ec03fe5162c097c25b5ad63c657ea1c7e94f1250
Tony Robertson <tonyr729@gmail.com>
2018-03-21 15:52:15 -0600

    Finish testing for platforms

--- test/Platform-test.js
+++ test/Platform-test.js
@@ -21,12 +21,33 @@ describe ('Game', function() {
         const platform = new Platform(15, 15, 15, 15);
         assert.deepEqual(platform.size, {w: 15, h: 15});
     });
-
-    it('should start with speed, gravity, and gravitySpeed of 0', function() {
+    
+    it('should be able to accelerate', function() {
         const platform = new Platform(15, 15, 15, 15);
-        assert.equal(platform.speed, 0);
-        assert.equal(platform.gravity, 0);
-        assert.equal(platform.gravitySpeed, 0);
+        platform.speed = 0;
+        platform.gravity = 0;
+
+        platform.accelerate(4, .2);
+
+        platform.speed = 4;
+        platform.gravity = .2;
     });
 
+    it('should freeze in place when stop function is called ', function() {
+        const platform = new Platform(15, 15, 15, 15);
+        platform.speed = 6;
+        platform.gravity = .2;
+        platform.gravitySpeed = 4;
+
+        platform.stop();
+
+        platform.speed = 0;
+        platform.gravity = 0;
+        platform.gravitySpeed = 0;
+    });
+
+    it('should have a size', function() {
+        const platform = new Platform(15, 15, 15, 15);
+        assert.deepEqual(platform.size, {w: 15, h: 15});
+    });
 });
